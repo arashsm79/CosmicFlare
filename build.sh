2054ed2b67cd31736ff6d235628d3131bb7a26fe
Arash Sal Moslehian <57039957+arashsm79@users.noreply.github.com>
2024-06-01 21:34:33 +0200

    build.sh: add cosmic desktop

--- build.sh
+++ build.sh
@@ -13,11 +13,18 @@ RELEASE="$(rpm -E %fedora)"
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-rpm-ostree install screen
+# Setup Copr repo
+wget https://copr.fedorainfracloud.org/coprs/ryanabx/cosmic-epoch/repo/fedora-40/ryanabx-cosmic-epoch-fedora-$(rpm -E %fedora).repo -O /etc/yum.repos.d/_copr_ryanabx-cosmic.repo
 
-# this would install a package from rpmfusion
-# rpm-ostree install vlc
+# Install cosmic desktop environment
+rpm-ostree install cosmic-desktop
 
-#### Example for enabling a System Unit File
+# Install extras (currently just a power manager and a libsecret manager)
+rpm-ostree install tuned gnome-keyring
 
+# Set up display manager
+rm -f /etc/systemd/system/display-manager.service
+ln -s /usr/lib/systemd/system/cosmic-greeter.service /etc/systemd/system/display-manager.service
+
+# Example for enabling a System Unit File
 systemctl enable podman.socket
